792d4edd2bd13d702e0467203922b9143e4dde9d
Stefan Thompson <elon43@yahoo.com>
2023-09-23 02:22:22 +0000

    `#13 migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
+# https://developer.hashicorp.com/terraform/tutorials/cloud/cloud-migrate
 # https://registry.terraform.io/providers/hashicorp/random/latest
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 terraform {
+  cloud {
+    organization = "tcbstefanthompson"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"
