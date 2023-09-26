3d79a357ea57b447ff4bc8f6c5178fbaaeda599c
Stefan Thompson <elon43@yahoo.com>
2023-09-26 02:10:51 +0000

    25 bucket should no longer user random provider

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "random" {
-  length  = 32
-  special = false
-  #lower = true
-  min_lower = 32
-}
-
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.random.result
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
   tags = {
-    UserUuid = "123456"
+    UserUuid = var.user_uuid
 
   }
 }
-
