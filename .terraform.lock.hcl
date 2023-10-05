0e4c2ea04a88f32963d562caf67bf8c7e8d58c23
elon43 <33724977+elon43@users.noreply.github.com>
2023-10-05 22:59:46 +0000

    44 terratowns provider (#47)
    * #44 WIP setting up the skeletonfor creating a custom terraform provider
    
    * #44 Implement terraform provider block in terraform
    
    * #44 WIP setting up the skeletonfor creating a custom terraform provider
    
    * #44 Setup skeleton for resource

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:jZ849Cxcat6FlQPhGTNIZU/mEpbSzKdICFHTxFBQ9Eo=",
+    "h1:OKCGGarQu07AqA6fZxFRZ3Gv/qVOup06D6Tt4zAMnwg=",
   ]
 }
--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -5,65 +5,115 @@ package main
 // import "fmt": Imports the fmt package, which contains functions for formatted I/O including functions
 // for printing to the console
 import (
-  "fmt"
- // "log"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
+
 // func main(): The entry point of the Go executable program.  The execution of the program starts here.
 func main() {
-  plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: Provider,
-
-
-  })
-  // Format.PrintLine
-  // Prints to standard output, and prints a new line character at the end
-  fmt.Println("Hello, World!")
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: Provider,
+	})
+	// Format.PrintLine
+	// Prints to standard output, and prints a new line character at the end
+	fmt.Println("Hello, World!")
 }
 
-//in golang, a title case function will get exported
+type Config struct {
+	Endpoint string
+	Token    string
+	UserUuid string
+}
+
+// in golang, a title case function will get exported
 func Provider() *schema.Provider {
-  var p *schema.Provider
-  p = &schema.Provider{
-    ResourcesMap: map[string]*schema.Resource{
-
-	},
-	DataSourcesMap: map[string]*schema.Resource{
-
-	},
-	Schema: map[string]*schema.Schema{
-	  "endpoint": {
-		Type: schema.TypeString,
-		Required: true,
-		Description: "The endpoint of the external service",
-
-	  },
-	  "token": {
-		Type: schema.TypeString,
-		Sensitive: true, //make the token as sensitive to hide it in the logs
-		Required: true,
-		Description: "The bearer token for authorization",
-	  },
-	  "user_uuid": {
-		Type: schema.TypeString,
-		Required: true,
-		Description: "UUID for configuration",
-		//ValidateFunc: validateUUID,
-	  },
-
-	},
-  }
-  //p.ConfigureContextFunc = providerConfigure(p)
-  return p
+	var p *schema.Provider
+	p = &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"terratowns_home": Resource(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{
+			"endpoint": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The endpoint of the external service",
+			},
+			"token": {
+				Type:        schema.TypeString,
+				Sensitive:   true, //make the token as sensitive to hide it in the logs
+				Required:    true,
+				Description: "The bearer token for authorization",
+			},
+			"user_uuid": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "UUID for configuration",
+				//		ValidateFunc: validateUUID,
+			},
+		},
+	}
+	p.ConfigureContextFunc = providerConfigure(p)
+	return p
 }
 
-// func validateUUID(v interface{}, k string) (ws []string, []error) {
-//   log.Print('validateUUID:start')
-//   value := v.(string)
-//   if _,err = uuid.Parse(value); err != nil {
-// 	errors = append(error, fmt.Errorf("invalid UUID format"))
-//   }
-//   log.Print('validateUUID:end')
-// }
+func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+	log.Print("validateUUID:start")
+	value := v.(string)
+	if _, err := uuid.Parse(value); err != nil {
+		errors = append(errors, fmt.Errorf("invalid UUID format"))
+	}
+	log.Print("validateUUID:end")
+	return
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureContextFunc {
+	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+		log.Print("providerConfigure:start")
+		config := Config{
+			Endpoint: d.Get("endpoint").(string),
+			Token:    d.Get("token").(string),
+			UserUuid: d.Get("user_uuid").(string),
+		}
+		log.Print("providerConfigure:end")
+		return &config, nil
+	}
+}
+
+func Resource() *schema.Resource {
+	log.Print("Resource:start")
+	resource := &schema.Resource{
+		CreateContext: resourceHouseCreate,
+		ReadContext:   resourceHouseRead,
+		UpdateContext: resourceHouseUpdate,
+		DeleteContext: resourceHouseDelete,
+	}
+	log.Print("Resource:start")
+	return resource
+}
+
+func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
+
+func resourceHouseDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	return diags
+}
