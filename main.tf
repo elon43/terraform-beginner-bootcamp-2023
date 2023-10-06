5d9987449acf7bab358aa30ba3503dcdfc4b9ebd
Stefan Thompson <elon43@yahoo.com>
2023-10-06 14:16:46 +0000

    #44 Finish implematation of terraform provider

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -30,3 +30,14 @@ provider "terratowns" {
 #  assets_path         = var.assets_path
 #  content_version     = var.content_version
 #}
+
+resource "terratowns_home" "home"{
+  name = "How to cook southern recipes in 2023!-updated"
+  description = <<DESCRIPTION
+Simple Description 
+DESCRIPTION
+  town = "cooker-cove"
+#  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "123456.cloudfront.net"
+  content_version = 1
+}
