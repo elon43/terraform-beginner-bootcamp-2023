0ea98a62fef1bd2f0428448069f23da1600c9cb2
Stefan Thompson <elon43@yahoo.com>
2023-10-11 04:55:15 +0000

    #49 test our terra home on terratowns test home

--- main.tf
+++ main.tf
@@ -16,28 +16,50 @@ terraform {
   #   }
 }
 
-provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
-}
-#module "terrahouse_aws" {
-#  source              = "./modules/terrahouse_aws"
-#  user_uuid           = var.user_uuid
-#  bucket_name         = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  assets_path         = var.assets_path
-#  content_version     = var.content_version
-#}
+// mock
+# provider "terratowns" {
+#   endpoint = "http://localhost:4567/api"
+#   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+#   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+# }
 
+# Used access code for taken AND user_uuid.  I entered the access code for my user_uuid when I signed up.
+provider "terratowns" {
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+#  user_uuid="8fd3b9cf-976e-4b54-be6a-aab0397d8a13" 
+  token = var.terratowns_access_token
+}
+
+
+module "terrahouse_aws" {
+ source              = "./modules/terrahouse_aws"
+ user_uuid           = var.teacherseat_user_uuid
+# bucket_name         = var.bucket_name
+ index_html_filepath = var.index_html_filepath
+ error_html_filepath = var.error_html_filepath
+ assets_path         = var.assets_path
+ content_version     = var.content_version
+}
+
+// mock
+# resource "terratowns_home" "home"{
+#   name = "Quick Easy Meal"
+#   description = <<DESCRIPTION
+# This is my go to meal when I want something quick, healthy,
+# and fast
+# DESCRIPTION    
+#   town = "cooker-cove"
+#   domain_name = "3fdq3gz.cloudfront.net"
+#   content_version = 1
+# }
+
+// town
 resource "terratowns_home" "home"{
-  name = "How to cook southern recipes in 2023!"
-  description = <<DESCRIPTION
-Simple Description 
-DESCRIPTION
-  town = "cooker-cove"
-#  domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "123456.cloudfront.net"
+  name = "How to cook southern recipes in 2023!-update"
+  description = "test"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
+#  domain_name = "1234.cloudfront.net"
   content_version = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
-variable "user_uuid" {
+variable "terratowns_endpoint" {
+  description = "The url for terratowns"
+  type        = string
+}
+
+variable "terratowns_access_token" {
+  description = "The access code"
+  type        = string
+}
+variable "teacherseat_user_uuid" {
   description = "The UUID of the user"
   type        = string
 }
-variable "bucket_name" {
-  description = "The name of the AWS S3 bucket"
-  type        = string
-}
+
+# variable "bucket_name" {
+#   description = "The name of the AWS S3 bucket"
+#   type        = string
+# }
 variable "index_html_filepath" {
   description = "This is the directory where the index.html file is located"
   type        = string
